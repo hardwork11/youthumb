11456f8b702d2f6ff5d87ea9b944ffe2af1bad29
mestrachi <143286716+Mestrachi@users.noreply.github.com>
2024-02-26 15:58:17 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,29 @@ class MyDocument extends Document {
             <h1>YouTube Thumbnail Downloader</h1>
             <p>▷ Save YT Video Thumbnails</p>
             <p>Get free thumbnail image of any youtube videos in Full HD(1080), HD (720), SD, and also in small size. it's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos</p>
-            {/* Add more language translations here */}
+            {/* How to use section */}
+            <h2>How to Use:</h2>
+            <p>I have shared some screenshots to guide you on how to use this YouTube downloader website. It's quite easy:</p>
+            <ol>
+              <li>Copy any YouTube link whose thumbnail you want.</li>
+              <li>Paste the URL in the input box. It will automatically generate different sizes of thumbnails.</li>
+              <li>Click on the thumbnail download button to download it to your system.</li>
+            </ol>
+            <p>If you are using an Android phone, you need to save the images. For iPhones, the process may vary.</p>
+            {/* Legal information */}
+            <h2>Legal Information:</h2>
+            <p>Of course, it's 100% legal to download YouTube video thumbnails. However, thumbnails and videos are copyrighted products, so you should take author permissions before reusing them.</p>
+            {/* Compatibility */}
+            <h2>Compatibility:</h2>
+            <p>This YouTube thumbnail downloader website works well on all devices except iPhone. It may not allow saving images due to device restrictions. Jailbroken iPhones should not face any issues. It works fine on Android devices and desktop systems.</p>
+            {/* Copyright */}
+            <h2>Copyright Information:</h2>
+            <p>Whatever YouTube screenshot you download is copyrighted by the respective owner of the videos. Always ask for permission before using it in your work.</p>
+            {/* SEO */}
+            <h2>SEO Considerations:</h2>
+            <p>Reusing YouTube thumbnails may not be SEO-friendly as most thumbnails on YouTube are indexed by Google. Adding unique effects using software like Photoshop can make them unique again and improve SEO friendliness.</p>
+            {/* Website information */}
+            <p><strong>YouTubThumbnailDownload.com - Copyright 2024</strong></p>
           </div>
           {/* Render the main content */}
           <Main />
