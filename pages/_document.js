a667dda9ce779ff5476044e3a266df77cbbe2665
mestrachi <143286716+Mestrachi@users.noreply.github.com>
2024-02-26 14:59:12 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,34 +12,32 @@ class MyDocument extends Document {
           <link rel="manifest" href="/favicon/site.webmanifest" />
           {/* Other meta tags */}
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="description" content="Your website description" />
-          <meta name="keywords" content="keyword1, keyword2, keyword3" />
+          <meta name="description" content="Get free thumbnail image of any YouTube videos in Full HD(1080), HD (720), SD, and also in small size. Currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos." />
+          <meta name="keywords" content="youtube thumbnail downloader, youtube thumbnail grabber, youtube thumbnail download, save youtube video thumbnail" />
           {/* CSS */}
           <link rel="stylesheet" href="/styles.css" />
           {/* Title */}
-          <title>Your Website Title</title>
+          <title>YouTube Thumbnail Downloader</title>
         </Head>
         <body>
-          {/* Language Selection */}
-          <div>
-            <p>Select Language:</p>
-            <ul>
-              <li><a href="/en">English</a></li>
-              <li><a href="/es">Spanish</a></li>
-              <li><a href="/fr">French</a></li>
-              {/* Add more languages as needed */}
-            </ul>
-          </div>
-
-          {/* Main content */}
           <Main />
-          {/* Next.js scripts */}
           <NextScript />
-
-          {/* Copyright notice with red background */}
-          <div style={{ backgroundColor: 'red', color: 'white', textAlign: 'center' }}>
-            <p>Copyright 2024 © youtubthumbnaildownload.com</p>
-            <p>Add this word to this symbol in a good way in half the background</p>
+          {/* Multilingual Content */}
+          <div id="multilingual-content">
+            {/* English */}
+            <div lang="en">
+              <h2>How to Use the YouTube Thumbnail Downloader</h2>
+              <p>Save free thumbnail images of any YouTube videos in Full HD (1080), HD (720), SD, and small sizes. Supported formats include YouTube (HD, HQ, 1080p, 4K) videos.</p>
+              <h3>Usage Instructions:</h3>
+              <ol>
+                <li>Copy the URL of the YouTube video whose thumbnail you want to download.</li>
+                <li>Paste the URL into the input box provided.</li>
+                <li>The tool will automatically generate different sizes of thumbnails.</li>
+                <li>Click on the thumbnail download button to download the image to your system.</li>
+              </ol>
+              <p>Please note that while downloading YouTube thumbnails is legal, it's essential to obtain permission from the video's owner if you plan to reuse the thumbnail.</p>
+            </div>
+            {/* Add content in other languages as needed */}
           </div>
         </body>
       </Html>
